80cb93f087af0a0e7e96c942bb9677e757d56552
Bruno Ambrozio <b.ambrozio@gmail.com>
2022-06-03 16:41:55 +0100

    Project created using Spring Initializr -> https://start.spring.io/

---
+++ src/main/java/com/blabs/javanf/JavanfApplication.java
@@ -0,0 +1,13 @@
+package com.blabs.javanf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavanfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavanfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blabs/javanf/JavanfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blabs.javanf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavanfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
